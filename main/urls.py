af7819e8054d1b778e7c05204dd163449ac9bc0e
demoh <demoh629@gmail.com>
2026-03-13 20:27:54 +0100

    add static in urls

--- main/urls.py
+++ main/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from django.conf import settings
@@ -69,4 +70,4 @@ urlpatterns = [
     path('ping', views.ping),
 
     path('media/<path:path>', serve, {'document_root': settings.MEDIA_ROOT}),
-] + debug_toolbar_urls()
+] + debug_toolbar_urls() + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
